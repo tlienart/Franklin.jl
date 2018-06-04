44111be5cc5b8af8c244a4939f8afdac28e236c1
Thibaut Lienart <tlienart@me.com>
2018-06-04 10:52:55 +1000

    fixed temporary files

--- test/t_misc.jl
+++ test/t_misc.jl
@@ -2,8 +2,8 @@ AAA = 5
 #
 # @test_throws AssertionError JuDoc.set_paths!() # PATH_INPUT undef
 #
-# td = tempdir() * "/"
-# PATH_INPUT = td
+td = mktempdir() * "/"
+PATH_INPUT = td
 #
 @testset "Extra utils" begin
 	# ifisdef
@@ -11,12 +11,12 @@ AAA = 5
  	@test AAA == 5
  	BBB = JuDoc.ifisdef(:BBB, 10)
  	@test BBB == 10
-#
-# 	JuDoc.set_paths!()
-# 	@test JuDoc.PATHS[:in] == td
-# 	@test JuDoc.PATHS[:in_css] == td * "_css/"
-# 	@test JuDoc.PATHS[:in_libs] == td * "_libs/"
-# 	@test JuDoc.PATHS[:out] == "web_output/"
-# 	@test JuDoc.PATHS[:out_css] == "web_output/_css/"
-# 	@test JuDoc.PATHS[:out_libs] == "web_output/_libs/"
+
+ 	JuDoc.set_paths!()
+ 	@test JuDoc.PATHS[:in] == td
+ 	@test JuDoc.PATHS[:in_css] == td * "_css/"
+ 	@test JuDoc.PATHS[:in_libs] == td * "_libs/"
+ 	@test JuDoc.PATHS[:out] == "web_output/"
+ 	@test JuDoc.PATHS[:out_css] == "web_output/_css/"
+ 	@test JuDoc.PATHS[:out_libs] == "web_output/_libs/"
 end
