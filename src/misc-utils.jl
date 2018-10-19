750e3406816378f000cdbd33d1c712e7d5b7cc80
Thibaut Lienart <tlienart@me.com>
2018-10-20 09:44:15 +1100

    cleaning up misc-utils, making sure from can play nicely with weird characters, tests passing

--- src/misc-utils.jl
+++ src/misc-utils.jl
@@ -1,16 +1,47 @@
-#=
-Convenience function to work with substring
-=#
+
+# Convenience functions to work with strings and substrings
+
+"""
+    str(s)
+
+Returns the string corresponding to `s`, `s` itself if it is a string, or the
+parent string if `s` is a substring. This does not allocate.
+"""
+str(s::String) = s
+str(s::SubString) = s.string
+
+
+"""
+    subs(s, from, to)
+    subs(s, from)
+    subs(s, range)
+
+Convenience functions to take a substring of a string. This only allocates for
+the creation of a substring object (i.e. extremely little).
+"""
 subs(s::AbstractString, from::Int, to::Int) = SubString(s, from, to)
 subs(s::AbstractString, from::Int) = subs(s, from, from)
 subs(s::AbstractString, range::UnitRange{Int}) = SubString(s, range)
 
-from(s::SubString) = s.offset + 1
-to(s::SubString) = s.offset + s.ncodeunits
 
-str(s::String) = s
-str(s::SubString) = s.string
+"""
+    from(ss)
 
+Given a substring `ss`, returns the position in the parent string where the
+substring starts.
+"""
+from(ss::SubString) = nextind(str(ss), ss.offset)
+
+
+"""
+    to(ss)
+
+Given a substring `ss`, returns the position in the parent string where the
+substring ends.
+"""
+to(ss::SubString) = ss.offset + ss.ncodeunits
+
+# Other convenience functions
 
 """
     time_it_took(start)
@@ -19,9 +50,9 @@ Convenience function to display a time since `start`.
 """
 function time_it_took(start)
     comp_time = time() - start
-    mess = comp_time > 60 ? "$(round(comp_time/60; digits=1))m" :
-           comp_time > 1 ? "$(round(comp_time; digits=1))s" :
-           "$(round(comp_time*1000; digits=1))ms"
+    mess = comp_time > 60 ? "$(round(comp_time/60;   digits=1))m" :
+           comp_time > 1 ?  "$(round(comp_time;      digits=1))s" :
+                            "$(round(comp_time*1000; digits=1))ms"
     mess = "✅ [done $mess]"
     println(mess)
 end
