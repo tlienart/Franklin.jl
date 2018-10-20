753f75c47121e71ec2e49d03d95648e2e2ee5b79
Thibaut Lienart <tlienart@me.com>
2018-10-20 16:57:42 +1100

    improving code quality for parser/tokens

--- src/parser/tokens.jl
+++ src/parser/tokens.jl
@@ -1,5 +1,16 @@
+"""
+    AbstractBlock
+
+This is the supertype defining a section of the string into consideration which
+can be considered as a "block". For instance, `abc { ... } def` contains one
+"braces block" corresponding to the substring `{ ... }`.
+All subtypes of AbstractBlock must have a `ss` field corresponding to the
+substring associated to the block.
+See also `Token`, `Block`, `OCBlock`....
+"""
 abstract type AbstractBlock end
 
+# convenience functions to locate the substring associated to a block
 from(β::AbstractBlock) = from(β.ss)
 to(β::AbstractBlock)   = to(β.ss)
 str(β::AbstractBlock)  = str(β.ss)
@@ -8,9 +19,12 @@ str(β::AbstractBlock)  = str(β.ss)
 """
     Token <: AbstractBlock
 
-A token `τ::Token` denotes the part of a main source string that we care about
-and identified by a symbol `τ.name` (e.g.: `:MATH_ALIGN_OPEN`). Tokens are
-typically used in this code to identify delimiters of environments.
+A token `τ::Token` denotes a part of the source string indicating a region that
+may need further processing. It is identified by a symbol `τ.name` (e.g.:
+`:MATH_ALIGN_OPEN`). Tokens are typically used in this code to identify
+delimiters of environments.
+For instance, `abc \$ ... \$ def` contains two tokens `:MATH_A` associated with
+the `\$` sign. Together they delimit here an inline math expression.
 See also `Block`, `LxBlock`.
 """
 struct Token <: AbstractBlock
@@ -25,12 +39,17 @@ end
 A `Block` has the same content as a `Token`, only the name indicates a segment
 of text typically surrounded by an opening and a closing token. This synonym
 helps the readability of the code to distinguish delimiters (tokens) from
-segments (blocks).
+segments (blocks) even though they're effectively defined the same way.
 See also `Token`, `LxBlock`.
 """
 const Block = Token
 
 
+"""
+    hblock(ss)
+
+Convenience function to mark a substring `ss` as a block with name `:H_BLOCK`.
+"""
 hblock(ss::SubString) = Block(:H_BLOCK, ss)
 
 
@@ -45,30 +64,53 @@ hblock_content(β::Block) = chop(β.ss, head=2, tail=2)
 """
     OCBlock
 
-Open-Close block, blocks that are defined by an opening token and a closing
-token, they may be nested. For instance braces block are formed of an
-opening `{` and a closing `}` and they could be nested.
+Open-Close block, blocks that are defined by an opening `Token` and a closing
+`Token`, they may be nested. For instance braces block are formed of an
+opening `{` and a closing `}` and could be nested.
 """
 struct OCBlock <: AbstractBlock
     name::Symbol
     ocpair::Pair{Token, Token}
     ss::SubString
 end
-OCBlock(name, ocpair) = OCBlock(name, ocpair,
-    subs(str(ocpair.first), from(ocpair.first), to(ocpair.second)))
 
+
+"""
+    OCBlock(name, ocpair)
+
+Shorthand constructor to instantiate an `OCBlock` inferring the associated
+substring from the `ocpair` (since it's the substring in between the tokens).
+"""
+OCBlock(η, ω) = OCBlock(η, ω, subs(str(ω.first), from(ω.first), to(ω.second)))
+
+
+"""
+    otok(ocb)
+
+Convenience function to retrieve the opening token of an `OCBlock`.
+"""
 otok(ocb::OCBlock) = ocb.ocpair.first
+
+
+"""
+    otok(ocb)
+
+Convenience function to retrieve the closing token of an `OCBlock`.
+"""
 ctok(ocb::OCBlock) = ocb.ocpair.second
+
 
 """
     content(ocb)
 
-Return the content of an open-close block (`OCBlock`), for instance the content
-of a `{...}` block would be `...`.
+Convenience function to return the content of an open-close block (`OCBlock`),
+for instance the content of a `{...}` block would be `...`.
 """
 function content(ocb::OCBlock)
-    s = str(ocb.ss)
-    return subs(s, nextind(s, to(otok(ocb))), prevind(s, from(ctok(ocb))))
+    s = str(ocb.ss) # this does not allocate
+    cfrom = nextind(s, to(otok(ocb)))
+    cto = prevind(s, from(ctok(ocb)))
+    return subs(s, cfrom, cto)
 end
 
 
@@ -99,37 +141,43 @@ const SPACER = [' ', '\n', '\t', '\v', '\f', '\r', '\u85', '\ua0', EOS]
     isexactly(refstring, follow, isfollowed)
 
 Forward lookup checking if a sequence of characters matches `refstring` and
-is followed (or not followed) by a character out of a list of characters
-(`follow`).
+is followed (or not followed if `isfollowed==false`) by a character out of a
+list of characters (`follow`).
 It returns
-* a number of steps indicating the number of characters to check
-* whether there is an offset or not (if it is required to check a following
-character or not)
-* a function that can be applied on a sequence of character.
+1. a number of steps indicating the number of characters to check,
+2. whether there is an offset or not (if it is required to check a following
+character or not),
+3. a function that can be applied on a sequence of character.
 """
 function isexactly(refstring::AbstractString, follow=Vector{Char}(),
                    isfollowed=true)
     # number of steps from the start character
-    steps = lastindex(refstring) - 1
+    steps = prevind(refstring, lastindex(refstring))
     # no offset (don't check next character)
     isempty(follow) && return (steps, false, s -> (s == refstring))
     # include next char for verification (--> offset of 1)
-    steps += 1
+    steps = nextind(refstring, steps)
     # verification function
-    λ(s) = begin
-        check = (s[end] ∈ follow)
-        (chop(s) == refstring) && ifelse(isfollowed, check, !check)
-    end
+    λ(s) = (chop(s) == refstring) && !xor(isfollowed, (s[end] ∈ follow))
+
     return (steps, true, λ)
 end
 
 
 """
+    α(c)
+
+Check whether `c` is a letter.
+"""
+α(c::Char) = isletter(c)
+
+
+"""
     α(c, ac)
 
-Check whether `c` is a letter or belongs to `ac`.
+Check whether `c` is in a vector of characters `ac`.
 """
-α(c::Char, ac=Vector{Char}()) = isletter(c) || (c ∈ ac)
+α(c::Char, ac::Vector{Char}) = (c ∈ ac)
 
 
 """
@@ -137,6 +185,8 @@ Check whether `c` is a letter or belongs to `ac`.
 
 Syntactic sugar for the incremental look case for which `steps=0` as well as
 the `offset`. This is a case where from a start character we lazily accept
-the next sequence of characters stopping as soon as one fails to verify `λ(c)`.
+the next sequence of characters stopping as soon as a character fails to verify
+`λ(c)`.
+See also `isexactly`.
 """
 incrlook(λ) = (0, false, λ)
