a1155f511147d39f2d87fd03b3d5e13fd1f1f6ca
Thibaut Lienart <tlienart@me.com>
2018-10-22 18:31:36 +1100

    airing of converter/html

--- src/converter/html.jl
+++ src/converter/html.jl
@@ -4,9 +4,11 @@
 Convert a judoc html string into a html string (i.e. replace {{ ... }} blocks).
 """
 function convert_html(hs::String, allvars=Dict{String, Pair{Any, Tuple}}())
+
     # Tokenize
     tokens = find_tokens(hs, HTML_TOKENS, HTML_1C_TOKENS)
     tokens = deactivate_blocks(tokens, HTML_ESCAPE)
+
     # Find hblocks ({{ ... }})
     hblocks, tokens = find_html_hblocks(tokens)
     # Find qblocks (qualify the hblocks)
@@ -17,6 +19,7 @@ function convert_html(hs::String, allvars=Dict{String, Pair{Any, Tuple}}())
     cdblocks, qblocks = find_html_cdblocks(qblocks)
     # Get the list of blocks to process
     hblocks = merge_blocks(qblocks, cblocks, cdblocks)
+
     # construct the final html
     pieces = Vector{AbstractString}()
     head = 1
@@ -28,6 +31,7 @@ function convert_html(hs::String, allvars=Dict{String, Pair{Any, Tuple}}())
     end
     strlen = lastindex(hs)
     (head < strlen) && push!(pieces, subs(hs, head, strlen))
+
     return prod(pieces)
 end
 
@@ -51,11 +55,13 @@ Helper function to process an individual block when the block is a `HFun`
 such as `{{ fill author }}`.
 """
 function convert_hblock(β::HFun, allvars::Dict)
+
     fname = lowercase(β.fname)
     haskey(JD_HBLOCKS, fname) && return JD_HBLOCKS[fname](β.params, allvars)
 
     # unknown function
     @warn "I found a function block '{{$fname ...}}' but I don't recognise this function name. Ignoring."
+
     return β.ss
 end
 
@@ -67,6 +73,7 @@ Helper function to process an individual block when the block is a `HCond`
 such as `{{ if showauthor }} {{ fill author }} {{ end }}`.
 """
 function convert_hblock(β::HCond, allvars::Dict)
+
     # check that the bool vars exist
     allconds = [β.init_cond, β.sec_conds...]
     has_else = (length(β.actions) == 1 + length(β.sec_conds) + 1)
@@ -78,6 +85,7 @@ function convert_hblock(β::HCond, allvars::Dict)
     else
         partial = β.actions[k]
     end
+
     return convert_html(String(partial), allvars)
 end
 
@@ -90,8 +98,10 @@ such as `{{ ifdef author }} {{ fill author }} {{ end }}`. Which checks
 if a variable exists and if it does, applies something.
 """
 function convert_hblock(β::HCondDef, allvars::Dict)
+
     hasvar = haskey(allvars, β.vname)
     doaction = ifelse(β.checkisdef, hasvar, !hasvar)
     doaction && return convert_html(String(β.action), allvars)
+
     return ""
 end
