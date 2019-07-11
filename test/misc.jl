3a519faa4a2f7654b5aed326d599c766703c7485
Cserteg Tamás <csertegt@gmail.com>
2019-07-11 13:17:57 +0200

    add nicer status messages when something printed between start and end

--- test/misc.jl
+++ test/misc.jl
@@ -67,7 +67,7 @@ end
     f = joinpath(d, "a.txt")
     open(f, "w") do outf
         redirect_stdout(outf) do
-            J.time_it_took(start)
+            J.printend("elapsing",start)
         end
     end
     r = read(f, String)
