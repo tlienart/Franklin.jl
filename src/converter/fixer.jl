6775283937ada1c4d8eb9f70c01f60d3cb873270
Thibaut Lienart <ltib@me.com>
2019-09-23 17:05:03 +0200

    more link fixing

--- src/converter/fixer.jl
+++ src/converter/fixer.jl
@@ -9,75 +9,49 @@ Direct inline-style links are properly processed by Julia's Markdown processor b
 """
 function find_and_fix_md_links(hs::String)::String
     # 1. find all occurences of -- [...]: link
-    # NOTE at this point things have already been preprocessed so
-    # [ was changed for &#91; and ] for &#93; and ! for &#33; making
-    # the regexes very readable...
 
-    # here we're looking for [id]: link; 1=id 2=link
-    m_link_defs = collect(eachmatch(r"&#91;((?:(?!&#93;).)*?)&#93;:\s+((?:(?!\<\/p\>)\S)+)", hs))
-
-    def_names = [def.captures[1] for def in m_link_defs]
-
-    # here's a trick, we do NOT use the link caught here; rather we check the dictionary
-    # PAGE_LINK_DEFS as otherwise the link may have been altered by JuDoc (e.g. if has underscores)
-    def_links = [PAGE_LINK_DEFS[def.captures[1]] for def in m_link_defs]
-
-    # here we're looking for [id] or [stuff][id] or ![stuff][id] but not [id]:
+    # here we're looking for [id] or [id][] or [stuff][id] or ![stuff][id] but not [id]:
+    # 1 > (&#33;)? == either ! or nothing
+    # 2 > &#91;(.*?)&#93; == [...] inside of the brackets
+    # 3 > (?:&#91;(.*?)&#93;)? == [...] inside of second brackets if there is such
     m_link_refs = collect(eachmatch(r"(&#33;)?&#91;(.*?)&#93;(?!:)(?:&#91;(.*?)&#93;)?", hs))
 
     # recuperate the appropriate name which has a chance to match def_names
-    ref_names = [ifelse(ref.captures[3] === nothing || isempty(ref.captures[3]),
-                    ref.captures[2], ref.captures[3]) for ref in m_link_refs]
-
-    # allocate a vector of associated definitions for each ref; if 0 nothing matched
-    assoc_def = zeros(Int, length(m_link_refs))
-
-    # aggregate and sort all matches
-    all_matches = vcat(m_link_defs, m_link_refs)
-    # stop early if there's none of that stuff
-    isempty(all_matches) && return hs
-    # sort by offset
-    sort!(all_matches, by = e -> e.offset)
-
-    # now for every ref, try to find an associated def; if none
-    # found then the relevant assoc_def will remain zero and that
-    # ref will be left in place as it was
-    for (i, refn) in enumerate(ref_names)
-        # check that there's a corresponding def, otherwise ignore
-        # we'll take the first match, it's up to the user not to have
-        # multiple definitions with the same id
-        j = findfirst(n->(n==refn), def_names)
-        j === nothing || (assoc_def[i] = j)
-    end
+    ref_names = [
+        # no second bracket or empty second bracket ?
+        # >> true then the id is in the first bracket
+        # >> false then the id is in the second bracket
+        ifelse(ref.captures[3] === nothing || isempty(ref.captures[3]),
+                    ref.captures[2], # first bracket
+                    ref.captures[3]) # second bracket
+                    for ref in m_link_refs]
 
     # reconstruct the text
     h = IOBuffer()
     head = 1
     i = 0
-    for m in all_matches
+    for (m, refn) in zip(m_link_refs, ref_names)
         # write what's before
         (head < m.offset) && write(h, subs(hs, head, prevind(hs, m.offset)))
-        # is it a def match or ref match?
-        if length(m.captures) == 3 # ref match
-            i  += 1
-            ref = m
-            # retrieve the index of the associated def
-            j = assoc_def[i]
-            if iszero(j)
-                write(h, m.match)
+        #
+        def = get(PAGE_LINK_DEFS, refn) do
+            ""
+        end
+        if isempty(def)
+            # no def found --> just leave it as it was
+            write(h, m.match)
+        else
+            if m.captures[3] !== nothing && isempty(m.captures[3])
+                # [link text][] indicating that the link text is the title
+                write(h, html_ahref(def, refn; title=refn))
             else
-                # write the link
-                if ref.captures[3] !== nothing && isempty(ref.captures[3])
-                    # [link text][] indicating that the link text is the title
-                    write(h, html_ahref(def_links[j], ref_names[i]; title=ref_names[i]))
+                if m.captures[1] !== nothing
+                    # ![alt][id]
+                    write(h, html_img(def, refn))
                 else
-                    if ref.captures[1] !== nothing # ![alt][id]
-                        write(h, html_img(def_links[j], ref.captures[2]))
-                    else
-                        # either [link text] and [link text]: ... elsewhere or
-                        # [link text][id] and [id]: ... later
-                        write(h, html_ahref(def_links[j], ref.captures[2]))
-                    end
+                    # either [link text] and [link text]: ... elsewhere or
+                    # [link text][id] and [id]: ... later
+                    write(h, html_ahref(def, refn))
                 end
             end
         end
