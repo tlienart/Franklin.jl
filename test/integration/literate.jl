221185204b7fe45bd1fa995570f514a63b0bae92
Thibaut Lienart <ltib@me.com>
2019-10-09 09:54:23 +0200

    adding test for \literate and showall

--- test/integration/literate.jl
+++ test/integration/literate.jl
@@ -71,4 +71,23 @@ end
         ```
 
         """
+
+    # Use of `\literate` command
+    h = raw"""
+        @def hascode = true
+        @def showall = true
+
+        \literate{/scripts/tutorial.jl}
+        """ |> jd2html_td
+    @test isapproxstr(h, """
+        <h1 id="rational_numbers"><a href="/index.html#rational_numbers">Rational numbers</a></h1>
+        <p>In julia rational numbers can be constructed with the <code>//</code> operator. Lets define two rational numbers, <code>x</code> and <code>y</code>:</p>
+        <pre><code class="language-julia"># Define variable x and y
+        x = 1//3
+        y = 2//5</code></pre>
+        <div class="code_output"><pre><code>2//5</code></pre></div>
+        <p>When adding <code>x</code> and <code>y</code> together we obtain a new rational number:</p>
+        <pre><code class="language-julia">z = x + y</code></pre>
+        <div class="code_output"><pre><code>11//15</code></pre></div>
+        """)
 end
