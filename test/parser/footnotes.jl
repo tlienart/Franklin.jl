f44c33092880f286dcf8f23050d97a827bf2cb83
Thibaut Lienart <ltib@me.com>
2019-10-19 11:25:44 +0200

    Fix sneaky issue with footnotes (#273)
    * # Fix issue with footnotes
    
    Footnote ref tokens were not deactivated properly in blocks that are meant to kill tokens within their span such as code blocks.
    
    * adding tests for the previous commit

--- test/parser/footnotes.jl
+++ test/parser/footnotes.jl
@@ -7,6 +7,7 @@
            <p>A<sup id="fnref:1"><a href="/index.html#fndef:1" class="fnref">[1]</a></sup>
               B<sup id="fnref:blah"><a href="/index.html#fndef:blah" class="fnref">[2]</a></sup>
               C</p>""")
+
     st = """
         A[^1] B[^blah]
         C
@@ -32,4 +33,20 @@
                     </tr>
                 </table>
             </p>""")
+end
+
+
+@testset "Fn in code" begin
+    s = raw"""
+        ```markdown
+       this has[^1]
+
+       [^1]: def
+       ```
+       blah
+       """
+    @test isapproxstr(s |> jd2html_td, """
+        <pre><code class="language-markdown">this has[^1]
+        [^1]: def
+        </code></pre> blah""")
 end
