e77a2239c444472e3b91a607b545e2ba499ba401
Thibaut Lienart <ltib@me.com>
2020-01-26 16:12:36 +0100

    to converter/html

--- test/converter/html2.jl
+++ test/converter/html2.jl
@@ -1,7 +1,7 @@
 @testset "Non-nested" begin
-    allvars = F.PageVars(
-        "a" => false => (Bool,),
-        "b" => false => (Bool,))
+    F.set_vars!(F.LOCAL_VARS, [
+        "a" => "false",
+        "b" => "false"])
 
     hs = """A{{if a}}B{{elseif b}}C{{else}}D{{end}}E"""
 
@@ -9,24 +9,25 @@
     hblocks, tokens = F.find_all_ocblocks(tokens, F.HTML_OCB)
     qblocks         = F.qualify_html_hblocks(hblocks)
 
-    F.set_vars!(allvars, ["a"=>"true","b"=>"false"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"true","b"=>"false"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ABE")
 
-    F.set_vars!(allvars, ["a"=>"false","b"=>"true"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"false","b"=>"true"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ACE")
 
-    F.set_vars!(allvars, ["a"=>"false","b"=>"false"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"false","b"=>"false"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ADE")
 end
 
 @testset "Nested" begin
-    allvars = F.PageVars(
-        "a" => false => (Bool,),
-        "b" => false => (Bool,),
-        "c" => false => (Bool,))
+    F.def_LOCAL_VARS!()
+    F.set_vars!(F.LOCAL_VARS, [
+        "a" => "false",
+        "b" => "false",
+        "c" => "false"])
 
     hs = """A {{if a}} B {{elseif b}} C {{if c}} D {{end}} {{else}} E {{end}} F"""
 
@@ -34,38 +35,39 @@ end
     hblocks, tokens = F.find_all_ocblocks(tokens, F.HTML_OCB)
     qblocks         = F.qualify_html_hblocks(hblocks)
 
-    F.set_vars!(allvars, ["a"=>"true"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"true"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ABF")
 
-    F.set_vars!(allvars, ["a"=>"false", "b"=>"true", "c"=>"false"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"false", "b"=>"true", "c"=>"false"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ACF")
 
-    F.set_vars!(allvars, ["a"=>"false", "b"=>"true", "c"=>"true"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"false", "b"=>"true", "c"=>"true"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "ACDF")
 
-    F.set_vars!(allvars, ["a"=>"false", "b"=>"false"])
-    fhs = F.process_html_qblocks(hs, allvars, qblocks)
+    F.set_vars!(F.LOCAL_VARS, ["a"=>"false", "b"=>"false"])
+    fhs = F.process_html_qblocks(hs, qblocks)
     @test isapproxstr(fhs, "AEF")
 end
 
 
 @testset "Bad cases" begin
+    F.def_LOCAL_VARS!()
     # Lonely End block
     s = """A {{end}}"""
-    @test_throws F.HTMLBlockError F.convert_html(s, F.PageVars())
+    @test_throws F.HTMLBlockError F.convert_html(s)
 
     # Inbalanced
     s = """A {{if a}} B {{if b}} C {{else}} {{end}}"""
-    @test_throws F.HTMLBlockError F.convert_html(s, F.PageVars())
+    @test_throws F.HTMLBlockError F.convert_html(s)
 
     # Some of the conditions are not bools
-    allvars = F.PageVars(
-        "a" => false => (Bool,),
-        "b" => false => (Bool,),
-        "c" => "Hello" => (String,))
+    F.set_vars!(F.LOCAL_VARS, [
+        "a" => "false",
+        "b" => "false",
+        "c" => "\"Hello\""])
     s = """A {{if a}} A {{elseif c}} B {{end}}"""
-    @test_throws F.HTMLBlockError F.convert_html(s, allvars)
+    @test_throws F.HTMLBlockError F.convert_html(s)
 end
