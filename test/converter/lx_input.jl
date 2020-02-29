15dc17edc2206ff9b54bd1b9aefc388fc18548a8
Thibaut Lienart <ltib@me.com>
2020-02-29 13:01:07 +0100

    a bit more coverage for latex io

--- test/converter/lx_input.jl
+++ test/converter/lx_input.jl
@@ -68,7 +68,7 @@ fs2()
     @test occursin("Finally img: <img src=\"/assets/index/code/output/s1a.png\" alt=\"\"> done.", h)
 end
 
-@testset  "Input MD" begin
+@testset "Input MD" begin
     mkpath(joinpath(F.PATHS[:site], "assets", "ccc"))
     fp = joinpath(F.PATHS[:site], "assets", "ccc", "asset1.md")
     write(fp, "blah **blih**")
@@ -77,4 +77,33 @@ end
         \textinput{ccc/asset1}
         """
     @test isapproxstr(st |> conv, "<p>Some string blah <strong>blih</strong></p>")
+end
+
+@testset "Input err" begin
+    gotd()
+    s = raw"""
+        AA
+        \input{julia}{foo/baz}
+        \input{plot}{foo/baz}
+        \textinput{foo/bar}
+        """ |> fd2html_td
+    @test isapproxstr(s, """
+        <p>AA
+        <p><span style="color:red;">// Couldn't find a file when trying to resolve an input request with relative path: `foo/baz`. //</span></p>
+        <p><span style="color:red;">// Couldn't find an output directory associated with 'foo/baz' when trying to input a plot. //</span></p>
+        <p><span style="color:red;">// Couldn't find a file when trying to resolve an input request with relative path: `foo/bar`. //</span></p></p>
+        """)
+
+    fs2()
+    gotd()
+    # table input
+    mkpath(joinpath(td, "_assets", "index", "output"))
+    write(joinpath(td, "_assets", "index", "output", "foo.csv"), "bar")
+    s = raw"""
+        @def fd_rpath = "index.md"
+        \tableinput{}{./foo.csv}
+        """ |> fd2html_td
+    @test isapproxstr(s, """
+        <p><span style="color:red;">// Table matching '/assets/index/foo.csv' not found. //</span></p></p>
+        """)
 end
