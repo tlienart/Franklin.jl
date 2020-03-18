e5fbaab46ea2b9f7dc65b24bf024649006304d73
Thibaut Lienart <ltib@me.com>
2020-03-18 10:40:26 +0100

    closes #412

---
+++ test/manager/page_vars_html.jl
@@ -0,0 +1,24 @@
+fs2()
+
+@testset "Scope (#412)" begin
+    write(joinpath(td, "config.md"), """
+        @def title = "config"
+        """)
+    write(joinpath(td, "page.md"), """
+        @def title = "page"
+        """)
+    write(joinpath(td, "index.html"), """
+        {{insert head.html}}
+        """)
+    mkpath(joinpath(td, "_layout"))
+    mkpath(joinpath(td, "_css"))
+    write(joinpath(td, "_layout", "head.html"), "<h1>{{fill title}}</h1>")
+    write(joinpath(td, "_layout", "page_foot.html"), "")
+    write(joinpath(td, "_layout", "foot.html"), "")
+    serve(single=true)
+
+    @test startswith(read(joinpath("__site", "index.html"), String),
+                        "<h1>config</h1>")
+    @test startswith(read(joinpath("__site", "page", "index.html"), String),
+                        "<h1>page</h1>")
+end
