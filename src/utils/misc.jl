56b78cde60b925e165f73efa933d3ed608ecd52a
Thibaut Lienart <tlienart@me.com>
2020-04-05 16:41:57 +0200

    Sync with FranklinTemplates update (#442)
    * synch with FranklinTemplates 06

--- src/utils/misc.jl
+++ src/utils/misc.jl
@@ -223,34 +223,6 @@ function rprint(s::AS)::Nothing
 end
 
 """
-    filecmp(path1, path2)
-
-Takes 2 absolute paths and check if the files are different.
-This code was suggested by Steven J. Johnson on discourse:
-https://discourse.julialang.org/t/how-to-obtain-the-result-of-a-diff-between-2-files-in-a-loop/23784/4
-"""
-function filecmp(path1::AbstractString, path2::AbstractString)
-    stat1, stat2 = stat(path1), stat(path2)
-    if !(isfile(stat1) && isfile(stat2)) || filesize(stat1) != filesize(stat2)
-        return false
-    end
-    stat1 == stat2 && return true # same file
-    open(path1, "r") do file1
-        open(path2, "r") do file2
-            buf1 = Vector{UInt8}(undef, 32768)
-            buf2 = similar(buf1)
-            while !eof(file1) && !eof(file2)
-                n1 = readbytes!(file1, buf1)
-                n2 = readbytes!(file2, buf2)
-                n1 != n2 && return false
-                0 != Base._memcmp(buf1, buf2, n1) && return false
-            end
-            return eof(file1) == eof(file2)
-        end
-    end
-end
-
-"""
     check_ping(ipaddr)
 
 Try a single ping to an address `ipaddr`. There's a timeout of 1s.
