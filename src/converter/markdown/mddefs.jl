372bfec45537b8157aa222cf964049df69ed579e
Thibaut Lienart <ltib@me.com>
2020-04-27 10:02:53 +0200

    .

--- src/converter/markdown/mddefs.jl
+++ src/converter/markdown/mddefs.jl
@@ -50,42 +50,42 @@ function process_mddefs(blocks::Vector{OCBlock}, isconfig::Bool,
         math = any(b -> b.name in MATH_BLOCKS_NAMES, blocks)
         set_var!(LOCAL_VARS, "hasmath", math)
     end
-    
+
     # copy the page vars to ALL_PAGE_VARS so that they can be accessed
     # by other pages via `pagevar`.
     ALL_PAGE_VARS[rpath] = deepcopy(LOCAL_VARS)
 
-    # TAGS
-    tags = Set(unique(locvar("tags")))
-    # Cases:
-    # 1. that page did not have tags
-    #   a. tags is empty --> do nothing
-    #   b. tags is not empty register them and update all
-    # 2. that page did have tags
-    #   a. tags are unchanged --> do nothing
-    #   b. check which ones change and update those
-    refresh_tags = tags
-    PAGE_TAGS    = globvar("fd_page_tags")
-    if isnothing(PAGE_TAGS)
-        isempty(tags) && return nothing
-        set_var!(GLOBAL_VARS, "fd_page_tags", DTAG((rpath => tags,)))
-    elseif !haskey(PAGE_TAGS, rpath)
-        isempty(tags) && return nothing
-        PAGE_TAGS[rpath] = tags
-    else
-        old_tags = PAGE_TAGS[rpath]
-        refresh_tags = setdiff(old_tags, tags)
-        isempty(refresh_tags) && return nothing
-        if isempty(tags)
-            delete!(PAGE_TAGS, rpath)
-        else
-            PAGE_TAGS[rpath] = tags
-        end
-    end
-    # In the full pass each page is processed first (without generating tag
-    # pages) and then, when all tags have been gathered, generate_tag_pages
-    # is called (see `fd_fullpass`).
-    # During the serve loop, we want to trigger on page change.
-    pagevar || FD_ENV[:FULL_PASS] || generate_tag_pages(refresh_tags)
+    # # TAGS
+    # tags = Set(unique(locvar("tags")))
+    # # Cases:
+    # # 1. that page did not have tags
+    # #   a. tags is empty --> do nothing
+    # #   b. tags is not empty register them and update all
+    # # 2. that page did have tags
+    # #   a. tags are unchanged --> do nothing
+    # #   b. check which ones change and update those
+    # refresh_tags = tags
+    # PAGE_TAGS    = globvar("fd_page_tags")
+    # if isnothing(PAGE_TAGS)
+    #     isempty(tags) && return nothing
+    #     set_var!(GLOBAL_VARS, "fd_page_tags", DTAG((rpath => tags,)))
+    # elseif !haskey(PAGE_TAGS, rpath)
+    #     isempty(tags) && return nothing
+    #     PAGE_TAGS[rpath] = tags
+    # else
+    #     old_tags = PAGE_TAGS[rpath]
+    #     refresh_tags = setdiff(old_tags, tags)
+    #     isempty(refresh_tags) && return nothing
+    #     if isempty(tags)
+    #         delete!(PAGE_TAGS, rpath)
+    #     else
+    #         PAGE_TAGS[rpath] = tags
+    #     end
+    # end
+    # # In the full pass each page is processed first (without generating tag
+    # # pages) and then, when all tags have been gathered, generate_tag_pages
+    # # is called (see `fd_fullpass`).
+    # # During the serve loop, we want to trigger on page change.
+    # pagevar || FD_ENV[:FULL_PASS] || generate_tag_pages(refresh_tags)
     return nothing
 end
