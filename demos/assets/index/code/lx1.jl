9c2ed7b6447c247e7a4b813512f405e8620cf6f4
tlienart <tlienart@me.com>
2020-10-20 11:10:00 +0000

    Deploying to gh-pages from  @ 3a93750c3e89eef061c6696eef47a3f2be338e4b 🚀

--- demos/assets/index/code/lx1.jl
+++
@@ -1,5 +0,0 @@
-# This file was generated, do not modify it. # hide
-using Latexify
-empty_ary = Array{Float32, 2}(undef, 2, 2)
-ls = latexify(empty_ary) # this is an L string
-println(ls.s) # hide
--- demos/assets/index/tcd1.jl
+++
@@ -1,7 +0,0 @@
-# This file was generated, do not modify it. # hide
-#hideall
-  save(SVG(joinpath(@OUTPUT, "tcd1.svg")),
-       TikzCD(raw"""A \arrow[r, "\phi"] \arrow[d, red]
-  & B \arrow[d, "\psi" red] \\
-  C \arrow[r, red, "\eta" blue]
-  & D"""))
