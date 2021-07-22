c38d869d48be60c37401b382ff92e530400f76c2
tlienart <tlienart@me.com>
2021-07-22 15:20:45 +0000

    Deploying to gh-pages from  @ 83455fe2191f55cd385af63a105aa260acc86e81 🚀

---
+++ demos/assets/wgl/code/ex.jl
@@ -0,0 +1,15 @@
+# This file was generated, do not modify it. # hide
+using WGLMakie, JSServe
+io = IOBuffer()
+println(io, "~~~")
+show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+app = JSServe.App() do
+    return DOM.div(
+        scatter(1:4),
+        surface(rand(4, 4)),
+        JSServe.Slider(1:3)
+    )
+end
+show(io, MIME"text/html"(), app)
+println(io, "~~~")
+println(String(take!(io)))
