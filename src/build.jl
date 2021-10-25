2261d48f6395e833c545caeb6b8f23680aa23825
Fredrik Ekre <ekrefredrik@gmail.com>
2021-10-25 10:27:12 +0200

    Initialize global variables lazily instead of during precompilation.

--- src/build.jl
+++ src/build.jl
@@ -1,4 +1,4 @@
-const PY = begin
+function PY()
     if "PYTHON3" ∈ keys(ENV)
         ENV["PYTHON3"]
     else
@@ -9,7 +9,7 @@ const PY = begin
         end
     end
 end
-const PIP = begin
+function PIP()
     if "PIP3" ∈ keys(ENV)
         ENV["PIP3"]
     else
@@ -20,7 +20,7 @@ const PIP = begin
         end
     end
 end
-const NODE = begin
+function NODE()
     if "NODE" ∈ keys(ENV)
         ENV["NODE"]
     else
@@ -39,8 +39,28 @@ Pre-rendering
 require `katex.min.js`
 - For highlights, we need `node` and also to have the `highlight.js` installed via `npm`.
 =#
-const FD_CAN_PRERENDER = shell_try(`$NODE -v`)
-const FD_CAN_HIGHLIGHT = shell_try(`$NODE -e "require('$(HIGHLIGHTJS[])')"`)
+function FD_CAN_PRERENDER()
+    r = shell_try(`$(NODE()) -v`)
+    if !r
+        @warn """Couldn't find node.js: `$(NODE()) -v` failed. Setting `prerender=false`.
+
+        Note: node is required for pre-rendering KaTeX and highlight.js, but it is not necessary to run Franklin (cf docs)."""
+    end
+    return r
+end
+let r = nothing # Hack to only run the checks once per call to Franklin.optimize since this is called multiple times
+    global function FD_CAN_HIGHLIGHT(; force::Bool=false)
+        if force || r === nothing
+            r = FD_CAN_PRERENDER() && shell_try(`$(NODE()) -e "require('$(HIGHLIGHTJS[])')"`)
+            if !r
+                @warn """Couldn't find highlight.js: `$(NODE()) -e "require('$(HIGHLIGHTJS[])')"` failed. Will not prerender code blocks.
+
+                Note: highlight.js is required for pre-rendering highlight.js, but is not necessary to run Franklin (cf docs)."""
+            end
+        end
+        return r
+    end
+end
 
 #=
 Minification
@@ -48,35 +68,26 @@ Minification
 - Here we check there is python3, and pip3, and then if we fail to import, we try to
 use pip3 to install it.
 =#
-const FD_HAS_PY3    = shell_try(`$([e for e in split(PY)]) -V`)
-const FD_HAS_PIP3   = shell_try(`$([e for e in split(PIP)]) -V`)
-const FD_CAN_MINIFY = FD_HAS_PY3 && FD_HAS_PIP3 &&
-        (success(`$([e for e in split(PY)]) -c "import css_html_js_minify"`) ||
-         success(`$([e for e in split(PIP)]) install css_html_js_minify`))
-#=
-Information to the user
-- what Franklin couldn't find
-- that the user should do a `build` step after installing
-=#
-FD_CAN_HIGHLIGHT || begin
-    FD_CAN_PRERENDER || begin
-        println("""✘ Couldn't find node.js (`$NODE -v` failed).
-                → It is required for pre-rendering KaTeX and highlight.js but is not necessary to run Franklin (cf docs).""")
-    end
-    println("""✘ Couldn't find highlight.js (`$NODE -e "require('$(HIGHLIGHTJS[])')"` failed).
-            → It is required for pre-rendering highlight.js but is not necessary to run Franklin (cf docs).""")
-end
+function FD_HAS_PY3()
+    r = shell_try(`$([e for e in split(PY())]) -V`)
+    if !r
+        @warn """Couldn't find python3 (`$([e for e in split(PY())]) -V` failed). Will not minify.
 
-FD_CAN_MINIFY || begin
-    if FD_HAS_PY3
-        println("✘ Couldn't find css_html_js_minify (`$([e for e in split(PY)]) -m \"import css_html_js_minify\"` failed).\n" *
-                """→ It is required for minification but is not necessary to run Franklin (cf docs).""")
-    else
-        println("""✘ Couldn't find python3 (`$([e for e in split(PY)]) -V` failed).
-                → It is required for minification but not necessary to run Franklin (cf docs).""")
+        Note: python3 is required for minification, but not necessary to run Franklin (cf docs)."""
     end
+    return r
 end
+FD_HAS_PIP3() = shell_try(`$([e for e in split(PIP())]) -V`)
 
-all((FD_CAN_HIGHLIGHT, FD_CAN_PRERENDER, FD_CAN_MINIFY, FD_HAS_PY3, FD_HAS_PIP3)) || begin
-    println("→ After installing any missing component, please re-build the package (cf docs).")
+function FD_CAN_MINIFY()
+    r = FD_HAS_PY3() && FD_HAS_PIP3()
+    r || return r
+    r = (success(`$([e for e in split(PY())]) -c "import css_html_js_minify"`) ||
+         success(`$([e for e in split(PIP())]) install css_html_js_minify`))
+    if !r
+        @warn """Couldn't find css_html_js_minify (`$([e for e in split(PY())]) -m "import css_html_js_minify"` failed). Will not minify.
+
+        Note: css_html_js_minify is required for minification, but is not necessary to run Franklin (cf docs)."""
+    end
+    return r
 end
