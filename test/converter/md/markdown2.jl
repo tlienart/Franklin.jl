23bb45e55dae8ed738b06c8d3f5fe873a4b8282a
Robert Moss <rmoss92@gmail.com>
2023-10-25 05:19:52 -0700

    Fixed TOC nesting with non-sequential headings (#1059)
    * Fixed TOC nesting with non-sequential headings
    
    * Skipped lists now appear as empty markers with proper indentation
    
    * Appropriately nest TOC

--- test/converter/md/markdown2.jl
+++ test/converter/md/markdown2.jl
@@ -31,33 +31,74 @@ end
     fs()
     h = raw"""
         @def fd_rpath = "pages/ff/aa.md"
+        @def mintoclevel = 1
+        @def maxtoclevel = 4
         \toc
-        ## Hello `fd`
-        #### weirdly nested
-        ### Goodbye!
-        ## Done
+        ### Part of family 1: should not be nested `fd`
+        ## Top-most ancestor of family 2
+        #### should be empty nested
+        ### part of family 2
+        ## Top-most ancestor of family 3
+        #### child1
+        #### child2
+        #### child3
+        #### child4
+        ## Top-most ancestor of family 4
         done.
         """ |> seval
     @test isapproxstr(h, raw"""
-        <div class="franklin-toc">
+          <div class="franklin-toc">
           <ol>
-            <li><a href="#hello_fd">Hello <code>fd</code></a>
+            <li>
+              <a href="#part_of_family_1_should_not_be_nested_fd">Part of family 1: should not be nested <code>fd</code></a>
+            </li>
+            <li>
+              <a href="#top-most_ancestor_of_family_2">Top-most ancestor of family 2</a>
               <ol>
-                <li>
+                <li style="list-style-type: none;">
                   <ol>
-                    <li><a href="#weirdly_nested">weirdly nested</a></li>
+                    <li>
+                      <a href="#should_be_empty_nested">should be empty nested</a>
+                    </li>
                   </ol>
                 </li>
-                <li><a href="#goodbye">Goodbye&#33;</a></li>
+                <li>
+                  <a href="#part_of_family_2">part of family 2</a>
+                </li>
               </ol>
             </li>
-            <li><a href="#done">Done</a></li>
+            <li>
+              <a href="#top-most_ancestor_of_family_3">Top-most ancestor of family 3</a>
+              <ol>
+                <li>
+                  <a href="#child1">child1</a>
+                </li>
+                <li>
+                  <a href="#child2">child2</a>
+                </li>
+                <li>
+                  <a href="#child3">child3</a>
+                </li>
+                <li>
+                  <a href="#child4">child4</a>
+                </li>
+              </ol>
+            </li>
+            <li>
+              <a href="#top-most_ancestor_of_family_4">Top-most ancestor of family 4</a>
+            </li>
           </ol>
         </div>
-        <h2 id="hello_fd"><a href="#hello_fd" class="header-anchor">Hello <code>fd</code></a></h2>
-        <h4 id="weirdly_nested"><a href="#weirdly_nested" class="header-anchor">weirdly nested</a></h4>
-        <h3 id="goodbye"><a href="#goodbye" class="header-anchor">Goodbye&#33;</a></h3>
-        <h2 id="done"><a href="#done" class="header-anchor">Done</a></h2>
+        <h3 id="part_of_family_1_should_not_be_nested_fd"><a href="#part_of_family_1_should_not_be_nested_fd" class="header-anchor">Part of family 1: should not be nested <code>fd</code></a></h3>
+        <h2 id="top-most_ancestor_of_family_2"><a href="#top-most_ancestor_of_family_2" class="header-anchor">Top-most ancestor of family 2</a></h2>
+        <h4 id="should_be_empty_nested"><a href="#should_be_empty_nested" class="header-anchor">should be empty nested</a></h4>
+        <h3 id="part_of_family_2"><a href="#part_of_family_2" class="header-anchor">part of family 2</a></h3>
+        <h2 id="top-most_ancestor_of_family_3"><a href="#top-most_ancestor_of_family_3" class="header-anchor">Top-most ancestor of family 3</a></h2>
+        <h4 id="child1"><a href="#child1" class="header-anchor">child1</a></h4>
+        <h4 id="child2"><a href="#child2" class="header-anchor">child2</a></h4>
+        <h4 id="child3"><a href="#child3" class="header-anchor">child3</a></h4>
+        <h4 id="child4"><a href="#child4" class="header-anchor">child4</a></h4>
+        <h2 id="top-most_ancestor_of_family_4"><a href="#top-most_ancestor_of_family_4" class="header-anchor">Top-most ancestor of family 4</a></h2>
         <p>done.</p>
         """)
 end
